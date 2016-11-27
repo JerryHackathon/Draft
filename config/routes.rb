554afb166470f9c734994a7750288e63093aa9af
charlesagyemang <micnkru@gmail.com>
2016-11-27 15:29:16 +0000

    I set my route files straight from resources to roots yh

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'uploads/index'
-
-  get 'uploads/new'
-
-  get 'uploads/create'
-
-  get 'uploads/destroy'
+	root 'resumes#index'
+	resources :uploads, only: [:index, :new, :create, :destroy] 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
