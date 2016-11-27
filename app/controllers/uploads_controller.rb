f3861f910efa3a5d331b78be751a191d666297a6
charlesagyemang <micnkru@gmail.com>
2016-11-27 15:44:32 +0000

    Completed the actions in my uploads controller

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,13 +1,29 @@
 class UploadsController < ApplicationController
   def index
+  	@uploads = Upload.all
   end
 
   def new
+  	@upload = Upload.new
   end
 
   def create
+  	@upload = Upload.new(upload_params)
+  	if @upload.save
+  		redirect_to uploads_path, notice: "The file #{@upload.filename}successfully uploaded"
+  	else
+  		render 'new'
+  	end	
   end
 
   def destroy
+  	@upload = Upload.find(params[:id])
+  	@upload.destroy
+  	redirect_to uploads_path, notice: "The file #{@upload.filename} has been successfully deleted"
+  end
+
+private
+  def upload_params
+  	params.require(:upload).permit(:filename, :attachment)
   end
 end
