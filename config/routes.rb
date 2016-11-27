0f9605fba2945dcc1d1f1769d632b03f326d7076
charlesagyemang <micnkru@gmail.com>
2016-11-27 15:19:22 +0000

    I created a controller for my Upload model and made rails create the following actions index create new and destroy for me

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'uploads/index'
+
+  get 'uploads/new'
+
+  get 'uploads/create'
+
+  get 'uploads/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
