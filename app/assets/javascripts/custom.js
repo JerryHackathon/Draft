4932bd52a48cd9d93a0d819f8e267af87789e8b4
charlesagyemang <micnkru@gmail.com>
2016-11-29 20:25:01 +0000

    I renamed the js to js.erb and explored the possibilities and finnaly settled on using ruby to route out the file and using js to do the auto draft selection.

--- app/assets/javascripts/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#groupings").hide()
-
-	$(".group_them").click(function(){
-		$("#groupings").toggle()
-
-	})
-})
--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,6 +1,6 @@
 module UploadsHelper
 	
-	def group_them (content, number)
+	def group_them (content, number=2)
 		h, count = [], 1
 		content.sample(content.length).each_slice(number) do |group_members|
 			result = ("Group #{count}\n#{group_members.join(", ")}\n")
@@ -32,4 +32,22 @@ end
 		<h2>Please upload a .txt file containing the names of indiduals you wish to put into groups / teams. Notice!, each name should be on a new line. Thanks for using auto_draft</h2>	
 	<% end %>
 </div>
+=end
+
+
+=begin
+<div id="groupings">
+	<% our_file = Upload.first %>
+	<% if our_file %>
+		<h2>Please refresh page and click on group to re-group teams</h2>
+		<% content = our_file.attachment.read.split("\n") %>
+		<% h = group_them(content, 8) %>
+		<% h.each do |teams| %>
+		<h3><%= teams.split("\n").first %></h3><br>
+		<h4><%= teams.split("\n").last %></h4>
+		<% end %>
+	<% else %>
+		<h2>Please upload a .txt file containing the names of individuals/items you wish to randomly put into groups / teams. Notice!, each name should be on a new line. Thanks for using auto_draft</h2>	
+	<% end %>	
+</div>		
 =end
